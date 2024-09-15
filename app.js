24a3882c329bc743601d2656e706ce1c308e42af
Michael Rodriguez <69970540+daviid0109@users.noreply.github.com>
2024-09-15 16:28:09 -0500

    Adding Js file

---
+++ app.js
@@ -0,0 +1,6 @@
+let tasks = []; //En este array se almacenaran las tareas
+let taskCount = 0; //Numero entero para contar tareas
+
+const welcomeMessage = `Bienvenido, has añadido ${taskCount} tareas.`; //creacion y manipulacion de strings
+
+console.log(welcomeMessage);
